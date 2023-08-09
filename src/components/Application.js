6aa5502bd478293e346eb97475760f22cce857e5
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-08-09 07:47:55 -0400

    creates useEffect hook to fetch days using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 const appointments = {
   "1": {
     id: 1,
@@ -61,6 +44,14 @@ const appointments = {
 };
 export default function Application(props) {
   const [day, setDay] = useState("Monday"); 
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    const URL = `http://localhost:8001/api/days`;
+    axios.get(URL).then(response => {
+      console.log(response.data);
+      setDays([...response.data]);
+    });
+  },[]);
   return (
     <main className="layout">
       <section className="sidebar">
